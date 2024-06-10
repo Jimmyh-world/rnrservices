38185294e234d1f38f5881ff11a00eab42f0b695
b21phiro <philip.rosenqvist@outlook.com>
2024-06-10 15:22:04 +0200

    Added logo, fonts and colors

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,10 @@
 <script setup>
 import '@/assets/css/components/header.css';
+import Logo from "@/components/Logo.vue";
 </script>
 
 <template>
-  <header id="header"></header>
+  <header id="header">
+    <Logo />
+  </header>
 </template>
---
+++ src/components/Logo.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import '@/assets/css/components/logo.css';
+</script>
+
+<template>
+  <div class="logo">
+    <img src="/logo-64-64.png" alt="Rock and Roll Services" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
