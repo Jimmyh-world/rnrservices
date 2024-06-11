a6d50c4d028db3129eb3bde5f465f6850c1fc1d4
b21phiro <philip.rosenqvist@outlook.com>
2024-06-11 12:08:35 +0200

    Fixed header during mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,22 +7,18 @@ import Logo from "@/components/Logo.vue";
 
 <template>
   <header id="header">
-    <div>
-      <Logo />
-    </div>
-    <div>
-      <a class="button" href="mailto:#" title="Mail">
-        <Mail />
-      </a>
-      <nav class="navigation">
-        <a href="#landing" title="Home">Home</a>
-        <a href="#services" title="Services">Services</a>
-        <a href="#testimonials" title="Testimonials">Testimonials</a>
-        <a href="#contact" title="Contact">Contact</a>
-      </nav>
-      <a class="button" href="mailto:#" title="Mail">
-        <Globe />
-      </a>
-    </div>
+    <Logo />
+    <a id="mailButton" class="button" href="mailto:#" title="Mail">
+      <Mail />
+    </a>
+    <nav class="navigation">
+      <a href="#landing" title="Home">Home</a>
+      <a href="#services" title="Services">Services</a>
+      <a href="#testimonials" title="Testimonials">Testimonials</a>
+      <a href="#contact" title="Contact">Contact</a>
+    </nav>
+    <a id="langButton" class="button" href="mailto:#" title="Mail">
+      <Globe />
+    </a>
   </header>
 </template>
