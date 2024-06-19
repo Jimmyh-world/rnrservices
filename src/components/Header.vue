bb360cae252e483397fb01a57d976674e5a85049
b21phiro <philip.rosenqvist@outlook.com>
2024-06-19 10:23:20 +0200

    Fixed header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,6 +6,7 @@ import Logo from "@/components/Logo.vue";
 import Language from "@/components/Language.vue";
 import text from "@/assets/js/text.js";
 import {ref} from "vue";
+import Menu from "@/components/icons/Menu.vue";
 
 const languageBoxShow = ref(false);
 
@@ -18,18 +19,23 @@ function toggleLanguageBox() {
 <template>
   <header id="header">
     <Logo />
-    <a id="mailButton" class="button" href="mailto:#" :title="text('Mail', 'Mejla')">
-      <Mail />
-    </a>
     <nav class="navigation">
       <a href="#landing" :title="text('Home', 'Hem')">{{ text("Home", "Hem") }}</a>
       <a href="#services" :title="text('Services', 'Service')">{{ text("Services", "Service") }}</a>
       <a href="#testimonials" :title="text('Testimonials', 'Rekommendationer')">{{ text('Testimonials', 'Rekommendationer') }}</a>
       <a href="#contact" :title="text('Contact', 'Kontakt')">{{ text('Contact', 'Kontakt') }}</a>
     </nav>
-    <button @click="toggleLanguageBox" id="langButton" class="button" :title="text('Select language', 'Välj språk')">
-      <Globe />
-    </button>
+    <div class="buttons">
+      <button @click="toggleLanguageBox" id="langButton" class="button" :title="text('Select language', 'Välj språk')">
+        <Globe />
+      </button>
+      <a id="mailButton" class="button" href="mailto:#" :title="text('Mail', 'Mejla')">
+        <Mail />
+      </a>
+      <button id="toggleMenuButton" class="button" title="Toggle menu">
+        <Menu />
+      </button>
+    </div>
     <Language :show="languageBoxShow" />
   </header>
 </template>
