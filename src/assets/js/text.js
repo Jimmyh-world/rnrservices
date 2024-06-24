5cf8ca76a73675fd174764babd4a94d89c75da76
b21phiro <philip.rosenqvist@outlook.com>
2024-06-24 16:23:24 +0200

    Cleaned repo

--- src/assets/js/text.js
+++
@@ -1,9 +0,0 @@
-export default (textEnglish, textSwedish = null) => {
-    switch (localStorage.getItem('preferred-language') ?? 'en') {
-        case 'sv':
-            return textSwedish ?? textEnglish;
-        case 'en':
-        default:
-            return textEnglish;
-    }
-};
--- src/views/NotFoundView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-
-</script>
-
-<template>
-  <main id="main">
-    <h1>Error 404</h1>
-  </main>
-</template>
