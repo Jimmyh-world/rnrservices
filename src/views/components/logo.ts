fd40ca7d683c0b08b3e87ba9b25657afb7221d7a
b21phiro <philip.rosenqvist@outlook.com>
2024-06-25 13:48:14 +0200

    init

--- src/views/components/logo.ts
+++
@@ -1,9 +0,0 @@
-import './../../css/modules/logo.css';
-
-export default (): string => {
-    return `
-    <div class="logo">
-        <a href="#" title="Rock & Roll Services">Rock & Roll Services</a>
-    </div>
-    `;
-}
--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import vitePluginFaviconsInject from 'vite-plugin-favicons-inject';
-import {defineConfig} from "vite";
-
-export default defineConfig({
-    base: "/rnrservices/",
-    plugins: [
-        vitePluginFaviconsInject('./src/images/logo.png'),
-    ]
-});
