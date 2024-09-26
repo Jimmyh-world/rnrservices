9f5dd82c165bb214b5af82791b8ea355d967cbe0
b21phiro <philip.rosenqvist@outlook.com>
2024-09-26 12:07:48 +0200

    Updated

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 import './style.css';
 
-import App from "./core/app.ts";
+import App from "./core/App.ts";
+import Logger from "./core/utilites/Logger.ts";
 
-App.init('#app');
+const logger = new Logger("Main");
+
+window.onload = () => {
+
+    const root = document.getElementById("app");
+
+    if (!root) {
+        logger.error("Root element not found");
+        return;
+    }
+
+    const app = App.getInstance(root);
+    app.run();
+
+};
